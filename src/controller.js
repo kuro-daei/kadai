b49abf4853b6fd504d847d7d98d1af7571f36df8
kuro-daei <eiji@kuroda.me>
2020-05-05 21:05:57 +0900

    init

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,22 @@
+import coins from "./coins";
 export default class Controller {
   constructor() {
-    this.name = "sample";
+    const button = document.getElementById("exec");
+    button.addEventListener(
+      "click",
+      () => {
+        const n = document.getElementById("n").value;
+        const max = 100;
+        const answers = coins(n, max);
+        for (let i = 0; i < answers.length; i++) {
+          const answer = answers[i];
+          const ans = document.getElementById("ans" + i);
+          ans.style.backgroundColor =
+            "rgba(128,128,255," + answer / (max * 2) + ")";
+          document.getElementById("ans" + i).value = answer;
+        }
+      },
+      false
+    );
   }
 }
---
+++ src/dice.js
@@ -0,0 +1,3 @@
+export default function dice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
