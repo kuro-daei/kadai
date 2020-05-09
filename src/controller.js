b3fe3c96554d30daf7d17038b783b8b5bd085dd2
kuro-daei <eiji@kuroda.me>
2020-05-09 17:18:53 +0900

    update

--- src/controller.js
+++ src/controller.js
@@ -3,12 +3,17 @@ import calc from "./calc";
 export default class Controller {
   constructor() {
     const equal = document.getElementById("equal");
-    equal.addEventListener("click", () => {
-      const minutes = parseInt(document.getElementById("minutes").value);
-      const answer = calc(minutes);
-      document.getElementById("ans_hours").value = answer.hours;
-      document.getElementById("ans_minutes").value = answer.minutes;
-      document.getElementById("ans_seconds").value = answer.seconds;
-    });
+    equal.addEventListener(
+      "click",
+      (event) => {
+        const seconds = parseInt(document.getElementById("seconds").value);
+        const answer = calc(seconds);
+        document.getElementById("ans_hours").value = answer.hours;
+        document.getElementById("ans_minutes").value = answer.minutes;
+        document.getElementById("ans_seconds").value = answer.seconds;
+        event.preventDefault();
+      },
+      false
+    );
   }
 }
